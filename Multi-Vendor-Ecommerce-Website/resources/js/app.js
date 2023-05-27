4789a78c5cf0464d49da250e26acfaa28aa4f2a4
rakibulhasan65 <rakibulhasan392364@gmail.com>
2023-05-27 23:49:43 +0600

    Vue Route

--- Multi-Vendor-Ecommerce-Website/resources/js/app.js
+++ Multi-Vendor-Ecommerce-Website/resources/js/app.js
@@ -1,12 +1,16 @@
 import "./bootstrap";
 import { createApp } from "vue";
+import router from "./index";
 
-const app = createApp({});
+const app = createApp({}).use(router).mount("#app");;
 import example from "./components/ExampleComponent.vue";
 import VueAdvance from "./components/VueAdvance.vue";
 import ApiTesting from "./components/ApiTesting.vue";
+import Home from "./components/views/Home.vue";
 app.component("example", example);
 app.component("VueAdvance", VueAdvance);
 app.component("ApiTesting", ApiTesting);
+app.component("Home", Home);
 
-app.mount("#app");
+// app.mount("#app");
+// app.use(router);
